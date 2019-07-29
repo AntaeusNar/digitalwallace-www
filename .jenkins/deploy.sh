4d14fe06233f549dd8ceb7bab9c7cca04c754a3e
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-07-29 13:33:04 -0700

    changed the agent in the jenkisfile and the deploy.sh script

--- .jenkins/deploy.sh
+++ .jenkins/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/deploy_id_rsa ubuntu@ec2-13-56-233-28.us-west-1.compute.amazonaws.com
+ssh -i ~/.ssh/deploy_id_rsa ubuntu@ec2-13-56-233-28.us-west-1.compute.amazonaws.com <<EOF
 
-cd "/git/digitalwallace-www"
-sudo git fetch --all
-sudo git checkout --force "origin/master"
-exit
+    cd "/git/digitalwallace-www"
+    sudo git fetch --all
+    sudo git checkout --force "origin/master"
+    exit
+EOF
